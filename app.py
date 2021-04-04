a432c7d7f7d72a97c6b749a2bee7c6a46ec83acd
JP23532 <haimin.hu@linecorp.com>
2021-04-04 18:44:56 +0900

    change index.html

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     name = "Hello World"
-    return render_template('hello.html', title='flask test', name=name)
+    return render_template('index.html', title='flask test', name=name)
 
 
-@app.route('/good')
+@app.route('/gotoauthpage')
 def good():
     name = "Good"
     return name
